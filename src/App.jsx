776962c52e1b1147ed4bc57748d56d1e4afe611b
pentakll4002 <dangthienan1207@gmail.com>
2025-04-23 12:10:18 +0700

    Create and fixed AppLayout.jsx, Header.jsx, MainNav.jsx and SideBar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import GlobalStyles from "./styles/GlobalStyles"
-import Dashboard from "./pages/Dashboard"; 
+import GlobalStyles from "./styles/GlobalStyles";
+import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
 import Account from "./pages/Account";
@@ -8,6 +8,7 @@ import Login from "./pages/Login";
 import Settings from "./pages/Settings";
 import Users from "./pages/Users";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
   return (
@@ -15,15 +16,18 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="/dashboard" />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/bookings' element={<Bookings />} />
-          <Route path='/cabins' element={<Cabins />} />
-          <Route path='/users' element={<Users />} />
-          <Route path='/settings' element={<Settings />} />
-          <Route path='/account' element={<Account />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='*' element={<PageNotFound />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="/dashboard" />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/bookings" element={<Bookings />} />
+            <Route path="/cabins" element={<Cabins />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/account" element={<Account />} />
+          </Route>
+
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,4 +1,7 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import { HiOutlineCog6Tooth, HiOutlineHome, HiOutlineHomeModern, HiOutlineUsers } from "react-icons/hi2";
+import { HiOutlineCalendar } from "react-icons/hi2";
 
 const NavList = styled.ul`
   display: flex;
@@ -6,7 +9,8 @@ const NavList = styled.ul`
   gap: 0.8rem;
 `;
 
-const Link = styled.a`
+// Using NavLink for Link
+const StyledNavLink = styled(NavLink)`
   &:link,
   &:visited {
     display: flex;
@@ -44,3 +48,45 @@ const Link = styled.a`
     color: var(--color-brand-600);
   }
 `;
+function MainNav() {
+  return (
+    <div>
+      <ul>
+        <NavList>
+          <li>
+            <StyledNavLink to="/dashboard">
+              <HiOutlineHome />
+              <span>Home</span>
+            </StyledNavLink>
+          </li>
+          <li>
+            <StyledNavLink to="/bookings">
+              <HiOutlineCalendar />
+              <span>Bookings</span>
+            </StyledNavLink>
+          </li>
+          <li>
+            <StyledNavLink to="/cabins">
+              <HiOutlineHomeModern />
+              <span>Cabins</span>
+            </StyledNavLink>
+          </li>
+          <li>
+            <StyledNavLink to="/users">
+              <HiOutlineUsers />
+              <span>Users</span>
+            </StyledNavLink>
+          </li>
+          <li>
+            <StyledNavLink to="/settings">
+              <HiOutlineCog6Tooth />
+              <span>Settings</span>
+            </StyledNavLink>
+          </li>
+        </NavList>
+      </ul>
+    </div>
+  );
+}
+
+export default MainNav;
